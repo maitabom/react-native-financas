ebb0157de3e163d9445e395e41b57426965751e3
Fábio Valentim <maitabom@gmail.com>
2025-08-31 19:09:53 -0300

    implementa tela de Login com campos de entrada e navegação para Cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,36 @@
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {
+  AreaInput,
+  Background,
+  Container,
+  Input,
+  Logo,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from './styles';
 
 export default function SignIn() {
+  const navigation = useNavigation<any>();
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Background>
+      <Container behavior="padding" enabled>
+        <Logo source={require('../../assets/Logo.png')} />
+        <AreaInput>
+          <Input placeholder="Seu e-mail" />
+        </AreaInput>
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+        <Link onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Criar uma conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
   );
 }
