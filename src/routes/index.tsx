067479bc8b0ab0036e6ed91913f045a6f26261b5
Fábio Valentim <maitabom@gmail.com>
2025-09-06 19:51:39 -0300

    adiciona navegação por drawer e integra com AuthContext para controle de rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,14 @@
-import { Text, View } from 'react-native';
 import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 function Routes() {
   const loading = false;
-  const signed = false;
 
-  return signed ? (
-    <View>
-      <Text>Teste</Text>
-    </View>
-  ) : (
-    <AuthRoutes />
-  );
+  const { signed } = useContext(AuthContext);
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 }
 
 export default Routes;
