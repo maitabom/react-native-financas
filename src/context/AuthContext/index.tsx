385923a9aba6e79079ae1608050ce9d7be6f551a
Fábio Valentim <maitabom@gmail.com>
2025-09-06 18:33:51 -0300

    refatora AuthContext para incluir signUp e ajustar tipos de contexto

--- src/context/AuthContext/index.tsx
+++ src/context/AuthContext/index.tsx
@@ -1,16 +1,37 @@
-import { createContext, useState } from "react";
-import AuthContextProperties from "./properties";
-import User from "../../models/user";
+import { createContext, useState } from 'react';
+import { AuthContextProperties, AuthProviderProperties } from './properties';
+import User from '../../models/user';
+import api from '../../services/api';
+import { useNavigation } from '@react-navigation/native';
 
-export const AuthContext = createContext<User>({ name: "" });
+export const AuthContext = createContext<AuthContextProperties>({
+  user: { name: '' },
+  signUp: async () => {},
+});
 
-function AuthProvider({children}: AuthContextProperties) {
+function AuthProvider({ children }: AuthProviderProperties) {
   const [user, setUser] = useState<User>({
-    name: "Valentim"
+    name: 'Valentim',
   });
 
+  const navigation = useNavigation();
+
+  async function signUp(name: string, email: string, password: string) {
+    try {
+      const response = await api.post('/users', { name, password, email });
+
+      if (response.status === 200 || response.status === 201) {
+        navigation.goBack();
+      } else {
+        console.warn('Cadastro não realizado', response.status);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={user}>
+    <AuthContext.Provider value={{ user, signUp }}>
       {children}
     </AuthContext.Provider>
   );
