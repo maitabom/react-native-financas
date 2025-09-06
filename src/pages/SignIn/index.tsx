164254214f153b59b0e98ef41d581a7cfadadbc1
Fábio Valentim <maitabom@gmail.com>
2025-09-06 20:22:48 -0300

    implementa tela de login com controle de estado para e-mail e senha

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -10,22 +10,48 @@ import {
   Link,
   LinkText,
 } from './styles';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { ActivityIndicator } from 'react-native';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn, loadingAuth } = useContext(AuthContext);
+
   const navigation = useNavigation<any>();
+
+  async function handleLogin() {
+    await signIn(email, password);
+  }
 
   return (
     <Background>
       <Container behavior="padding" enabled>
         <Logo source={require('../../assets/Logo.png')} />
         <AreaInput>
-          <Input placeholder="Seu e-mail" />
+          <Input
+            placeholder="Seu e-mail"
+            keyboardType="email-address"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
         </AreaInput>
         <AreaInput>
-          <Input placeholder="Sua senha" />
+          <Input
+            placeholder="Sua senha"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
         </AreaInput>
-        <SubmitButton activeOpacity={0.8}>
-          <SubmitText>Acessar</SubmitText>
+        <SubmitButton activeOpacity={0.8} onPress={handleLogin}>
+          {loadingAuth ? (
+            <ActivityIndicator size={20} color="#FFF" />
+          ) : (
+            <SubmitText>Acessar</SubmitText>
+          )}
         </SubmitButton>
         <Link onPress={() => navigation.navigate('SignUp')}>
           <LinkText>Criar uma conta</LinkText>
