830338f6864b930172d598ca38ed887009a445a8
Fábio Valentim <maitabom@gmail.com>
2025-09-07 19:19:28 -0300

    adiciona controle de loading na navegação e cria componente Container para estilização

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,20 @@ import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { Container } from './styles';
+import { ActivityIndicator } from 'react-native';
 
 function Routes() {
-  const loading = false;
 
-  const { signed } = useContext(AuthContext);
+  const { signed, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <Container>
+        <ActivityIndicator size="large" color='#141414' />
+      </Container>
+    );
+  }
 
   return signed ? <AppRoutes /> : <AuthRoutes />;
 }
