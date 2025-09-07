f6219073b513b1570e6828004f86e2071befebae
Fábio Valentim <maitabom@gmail.com>
2025-09-07 20:22:39 -0300

    remove lógica de autenticação da página Home e adiciona componente Header com fundo estilizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
-import { useContext } from 'react';
-import { Button, Text, View } from 'react-native';
-import { AuthContext } from '../../context/AuthContext';
+import { Background } from './styles';
+import Header from '../../components/Header';
 
 export default function Home() {
-  const { signOut } = useContext(AuthContext);
+
 
   return (
-    <View>
-      <Text>Bem-vindo ao sistema</Text>
-      <Button title="Sair" onPress={() => signOut()} />
-    </View>
+    <Background>
+      <Header title='Minhas movimentações'/>
+    </Background>
   );
 }
