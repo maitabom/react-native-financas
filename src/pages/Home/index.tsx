02d66c6b3f7c5e937eef2d770a277fc485c6284a
Fábio Valentim <maitabom@gmail.com>
2025-09-07 20:50:50 -0300

    adiciona lógica para obter e exibir o saldo de movimentações na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,42 @@
 import { Background } from './styles';
+import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
 import Header from '../../components/Header';
+import api from '../../services/api';
+import ResumeTransaction from '../../models/resume-transaction';
 
 export default function Home() {
+  const [listBalance, setListBalance] = useState<ResumeTransaction[]>([]);
+  const [dateMoviments, setDateMoviments] = useState<Date>(new Date());
 
+  useEffect(() => {
+    let isActive = true;
+
+    async function getMoviments() {
+      let dateFormat = format(dateMoviments, 'dd/MM/yyyy');
+
+      const response = await api.get<ResumeTransaction[]>('/balance', {
+        params: {
+          date: dateFormat,
+        },
+      });
+
+      if (isActive) {
+        const balances = response.data;
+        setListBalance(balances);
+      }
+    }
+
+    getMoviments();
+
+    return () => {
+      isActive = false;
+    };
+  });
 
   return (
     <Background>
-      <Header title='Minhas movimentações'/>
+      <Header title="Minhas movimentações" />
     </Background>
   );
 }
