e0e4010aff19a2f20cce6f13e7ab2b868d2a6c0a
Fábio Valentim <maitabom@gmail.com>
2025-09-13 17:21:52 -0300

    adiciona componente ListBalance e integra BalanceItem na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,17 @@
-import { Background } from './styles';
+import { Background, ListBalance } from './styles';
 import { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import Header from '../../components/Header';
 import api from '../../services/api';
 import ResumeTransaction from '../../models/resume-transaction';
+import { useIsFocused } from '@react-navigation/native';
+import BalanceItem from '../../components/BalanceItem';
 
 export default function Home() {
   const [listBalance, setListBalance] = useState<ResumeTransaction[]>([]);
   const [dateMoviments, setDateMoviments] = useState<Date>(new Date());
+
+  const isFocus = useIsFocused();
 
   useEffect(() => {
     let isActive = true;
@@ -37,6 +41,15 @@ export default function Home() {
   return (
     <Background>
       <Header title="Minhas movimentações" />
+      <ListBalance
+        data={listBalance}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={item => (item as ResumeTransaction).tag}
+        renderItem={({ item }) => (
+          <BalanceItem data={item as ResumeTransaction} />
+        )}
+      />
     </Background>
   );
 }
