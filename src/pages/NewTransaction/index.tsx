e571e7883ed5810973e5e4bc290a1360e97f4882
Fábio Valentim <maitabom@gmail.com>
2025-09-13 19:19:12 -0300

    adiciona validação e confirmação de dados na tela de Nova Transação

--- src/pages/NewTransaction/index.tsx
+++ src/pages/NewTransaction/index.tsx
@@ -1,9 +1,17 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/Header';
 import { Background, Input, SubmitButton, SubmitText } from './styles';
-import { Keyboard, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import {
+  Alert,
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { useState } from 'react';
 import TransactionTypes from '../../components/TransactionTypes';
+import api from '../../services/api';
+import { format } from 'date-fns';
+import { useNavigation } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   safeArea: {
@@ -18,6 +26,55 @@ export default function NewTransacion() {
   const [labelInput, setLabelInput] = useState('');
   const [valueInput, setValueInput] = useState('');
   const [type, setType] = useState<TypeTransaction>('receita');
+
+  const navigation = useNavigation<any>();
+
+  function handleSubmit() {
+    Keyboard.dismiss();
+
+    if (labelInput === '' || isNaN(parseFloat(valueInput)) || type === null) {
+      Alert.alert('Atenção', 'Preencha todos os campos', [
+        { text: 'OK', style: 'cancel' },
+      ]);
+      return;
+    }
+
+    Alert.alert(
+      'Confirme os dados',
+      `A transação do tipo ${type}, ${labelInput} de valor ${parseFloat(
+        valueInput,
+      ).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      })}. Confirma o registro?`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleSave(),
+        },
+      ],
+    );
+  }
+
+  async function handleSave() {
+    Keyboard.dismiss();
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy'),
+    });
+
+    setLabelInput('');
+    setValueInput('');
+
+    navigation.navigate('Home');
+  }
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss}>
@@ -36,9 +93,9 @@ export default function NewTransacion() {
             onChangeText={text => setValueInput(text)}
           />
 
-          <TransactionTypes type={type} onTypeChanged={(item) => setType(item)} />
+          <TransactionTypes type={type} onTypeChanged={item => setType(item)} />
 
-          <SubmitButton activeOpacity={0.8}>
+          <SubmitButton activeOpacity={0.8} onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </SafeAreaView>
